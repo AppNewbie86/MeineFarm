57ddeaf9f7e6971683d86ca1791c8cf4160ea90a
Marcel Zimmermann <mzmann252@gmail.com>
2023-12-26 12:03:49 +0100

    Bug Fixxed

---
+++ MeineFarm/Views/Terms/TermsOfServiceView.swift
@@ -0,0 +1,18 @@
+//
+//  TermsOfServiceView.swift
+//  MeineFarm
+//
+//  Created by Marcel Zimmermann on 26.12.23.
+//
+
+import SwiftUI
+
+struct TermsOfServiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermsOfServiceView()
+}
